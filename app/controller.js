717cd5fba46675e4165620f22db54170b3ec768b
lukeschunk <luke.schunk@mx.com>
2017-02-11 10:29:24 -0700

    pages togglling on nav click

--- app/controller.js
+++ app/controller.js
@@ -1,3 +1,18 @@
 angular.module('project').controller('mainController', function($scope) {
-  $scope.test = 'its working';
+
+  var pages = ['view', 'find', 'update'];
+
+  $scope.view = true;
+  $scope.find = false;
+  $scope.update = false;
+
+  $scope.togglePage = function (clickedNavView) {
+    pages.forEach(function(page) {
+      if (page === clickedNavView) {
+        $scope[page] = true;
+      } else {
+        $scope[page] = false;
+      }
+    })
+  }
 })
